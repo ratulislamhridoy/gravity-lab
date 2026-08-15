712c2d10d24de684cec65cf9bbef925921cf84e8
ratulislamhridoy <ratulislamhridoy@gmail.com>
2026-08-15 20:58:58 +0600

    Enhance client-side vectorizer with instant SVG fallback

--- app.js
+++ app.js
@@ -3740,38 +3740,48 @@ Synthesize these visual properties and stylistic DNA into your generated prompt 
   }
 
   function vectorizeTileClientSide(tile, mode, fillColor, smoothing, corner, simplify, speckle) {
+    if (!tile || !tile.dataUrl) {
+      const emptySvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100%" height="100%"></svg>`;
+      window.handleVectorizeTileResult({ ok: true, index: tile ? tile.idx : 1, svg: emptySvg });
+      return;
+    }
+
     const img = new Image();
-    img.crossOrigin = 'Anonymous';
     img.onload = function() {
+      const w = img.width || 128;
+      const h = img.height || 128;
       const cvs = document.createElement('canvas');
-      cvs.width = img.width || 200;
-      cvs.height = img.height || 200;
+      cvs.width = w;
+      cvs.height = h;
       const ctx = cvs.getContext('2d');
       ctx.drawImage(img, 0, 0);
-      let imgData;
+
+      let imgData = null;
       try {
-        imgData = ctx.getImageData(0, 0, cvs.width, cvs.height);
+        imgData = ctx.getImageData(0, 0, w, h);
       } catch (e) {
         console.warn('Canvas getImageData notice:', e);
       }
 
-      if (imgData && window.vectorWorkerManager) {
-        window.vectorWorkerManager.vectorizeTile(imgData, cvs.width, cvs.height, { threshold: 128 }).then(res => {
+      if (imgData && window.vectorWorkerManager && window.vectorWorkerManager.worker) {
+        window.vectorWorkerManager.vectorizeTile(imgData, w, h, { threshold: 128 }).then(res => {
           const svgString = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${res.width} ${res.height}" width="100%" height="100%"><path fill="${fillColor || '#344e41'}" d="${res.pathD}"/></svg>`;
           window.handleVectorizeTileResult({ ok: true, index: tile.idx, svg: svgString });
-        }).catch(err => {
-          const fallbackSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${cvs.width} ${cvs.height}" width="100%" height="100%"><image href="${tile.dataUrl}" width="${cvs.width}" height="${cvs.height}"/></svg>`;
+        }).catch(() => {
+          const fallbackSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${w} ${h}" width="100%" height="100%"><image href="${tile.dataUrl}" width="${w}" height="${h}"/></svg>`;
           window.handleVectorizeTileResult({ ok: true, index: tile.idx, svg: fallbackSvg });
         });
       } else {
-        const fallbackSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${cvs.width} ${cvs.height}" width="100%" height="100%"><image href="${tile.dataUrl}" width="${cvs.width}" height="${cvs.height}"/></svg>`;
+        const fallbackSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${w} ${h}" width="100%" height="100%"><image href="${tile.dataUrl}" width="${w}" height="${h}"/></svg>`;
         window.handleVectorizeTileResult({ ok: true, index: tile.idx, svg: fallbackSvg });
       }
     };
+
     img.onerror = function() {
-      const fallbackSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" width="100%" height="100%"><image href="${tile.dataUrl}" width="200" height="200"/></svg>`;
+      const fallbackSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128" width="100%" height="100%"><image href="${tile.dataUrl}" width="128" height="128"/></svg>`;
       window.handleVectorizeTileResult({ ok: true, index: tile.idx, svg: fallbackSvg });
     };
+
     img.src = tile.dataUrl;
   }
 
