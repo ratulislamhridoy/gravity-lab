9631f111d22c62b4f9cd1978f378c6ab75ce0314
ratulislamhridoy <ratulislamhridoy@gmail.com>
2026-08-22 12:49:52 +0600

    Fix offline user subscription reversion by auto-inserting missing users in update-plan API

--- api/users/update-plan.js
+++ api/users/update-plan.js
@@ -33,12 +33,31 @@ module.exports = async (req, res) => {
           { email: { $regex: new RegExp('^' + (email || '').replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&') + '$', 'i') } }
         ]
       };
-      await col.updateOne(query, {
-        $set: {
+      const existing = await col.findOne(query);
+      if (existing) {
+        await col.updateOne({ _id: existing._id }, {
+          $set: {
+            subscription: plan,
+            subscriptionExpiry: expiry
+          }
+        });
+      } else {
+        const now = new Date().toISOString();
+        await col.insertOne({
+          uid: uid,
+          email: email || '',
+          displayName: email ? email.split('@')[0] : 'User',
+          photoURL: 'https://lh3.googleusercontent.com/a/default-user',
+          provider: 'google.com',
+          status: 'active',
+          lastActive: now,
+          firstLogin: now,
+          metrics: {},
           subscription: plan,
-          subscriptionExpiry: expiry
-        }
-      });
+          subscriptionExpiry: expiry,
+          creditsDaily: { remaining: 15, lastResetDate: now.split('T')[0] }
+        });
+      }
     } else {
       // File Fallback
       let users = loadMongoUsersLocal();
@@ -46,8 +65,24 @@ module.exports = async (req, res) => {
       if (index >= 0) {
         users[index].subscription = plan;
         users[index].subscriptionExpiry = expiry;
-        saveMongoUsersLocal(users);
+      } else {
+        const now = new Date().toISOString();
+        users.push({
+          uid: uid,
+          email: email || '',
+          displayName: email ? email.split('@')[0] : 'User',
+          photoURL: 'https://lh3.googleusercontent.com/a/default-user',
+          provider: 'google.com',
+          status: 'active',
+          lastActive: now,
+          firstLogin: now,
+          metrics: {},
+          subscription: plan,
+          subscriptionExpiry: expiry,
+          creditsDaily: { remaining: 15, lastResetDate: now.split('T')[0] }
+        });
       }
+      saveMongoUsersLocal(users);
     }
 
     res.status(200).json({ ok: true, message: 'User subscription updated' });
--- server.js
+++ server.js
@@ -482,12 +482,31 @@ const server = http.createServer((req, res) => {
               { email: { $regex: new RegExp('^' + (email || '').replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&') + '$', 'i') } }
             ]
           };
-          await col.updateOne(query, {
-            $set: {
+          const existing = await col.findOne(query);
+          if (existing) {
+            await col.updateOne({ _id: existing._id }, {
+              $set: {
+                subscription: plan,
+                subscriptionExpiry: expiry
+              }
+            });
+          } else {
+            const now = new Date().toISOString();
+            await col.insertOne({
+              uid: uid,
+              email: email || '',
+              displayName: email ? email.split('@')[0] : 'User',
+              photoURL: 'https://lh3.googleusercontent.com/a/default-user',
+              provider: 'google.com',
+              status: 'active',
+              lastActive: now,
+              firstLogin: now,
+              metrics: {},
               subscription: plan,
-              subscriptionExpiry: expiry
-            }
-          });
+              subscriptionExpiry: expiry,
+              creditsDaily: { remaining: 15, lastResetDate: now.split('T')[0] }
+            });
+          }
         } else {
           // File Fallback
           let users = loadMongoUsersLocal();
@@ -495,8 +514,24 @@ const server = http.createServer((req, res) => {
           if (index >= 0) {
             users[index].subscription = plan;
             users[index].subscriptionExpiry = expiry;
-            saveMongoUsersLocal(users);
+          } else {
+            const now = new Date().toISOString();
+            users.push({
+              uid: uid,
+              email: email || '',
+              displayName: email ? email.split('@')[0] : 'User',
+              photoURL: 'https://lh3.googleusercontent.com/a/default-user',
+              provider: 'google.com',
+              status: 'active',
+              lastActive: now,
+              firstLogin: now,
+              metrics: {},
+              subscription: plan,
+              subscriptionExpiry: expiry,
+              creditsDaily: { remaining: 15, lastResetDate: now.split('T')[0] }
+            });
           }
+          saveMongoUsersLocal(users);
         }
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify({ ok: true, message: 'Plan updated' }));
